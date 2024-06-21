deb7a7a6c10e08aac703592aec140cd6b62db1d1
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2024-06-20 18:06:42 -0600

    Add files via upload

--- gta_sa_ProfileSet.py
+++ gta_sa_ProfileSet.py
@@ -1,8 +1,6 @@
 from Modulos.Modulo_System import *
-from Modulos.Modulo_Files import *
 from Modulos.Modulo_Language import *
 from Modulos.Modulo_ShowPrint import *
-from Modulos.Modulo_Text import *
 from pathlib import Path as pathlib
 import os, subprocess
 
@@ -10,6 +8,77 @@ from Modulos.gta_modloader_function import *
 
 
 
+
+'''
+Funcion que convierte texto a numero, o a una lista de numeros.
+Solo si se escriben caracteres tipo numero, o el caracter "," o espacio " "
+'''
+def input_text_to_number(text='0', list_mode=True):
+    # Solo se dovolveran numeros, el texto solo incluye estos caracteres
+    only_include = '0123456789'
+    character_coma = ','
+    character_space = ' '
+    only_include += character_coma
+    only_include += character_space
+
+    # Determinar si todos los caracteres, se incluyen en "only_include"
+    # Determinar si hay comas, si las hay se listaran los numeros
+    list_text = []
+    for character in text:
+        list_text.append(False)
+
+    coma = False
+    number = 0
+    for character in text:
+        for character_oi in only_include:
+            if character_oi == character:
+                list_text[number] = True
+                if character == character_coma:
+                    coma = True
+        number += 1
+
+    if coma == True and list_mode == False:
+        coma = False
+        text = text.replace(character_coma, '')
+
+    input_good = True
+    for bool in list_text:
+        if bool == False:
+            input_good = False
+
+    # Devolver de texto a numero o no
+    if input_good == True:
+        text = text.replace(character_space, '')
+
+        if text.replace(character_coma, '') == '':
+            go = False
+        else:
+            go = True
+
+        if go == True:
+            if coma == True:
+                numbers = []
+                for number in text.split(character_coma):
+                    numbers.append( int(number) )
+
+                return numbers
+            else:
+                return int(text)
+        else:
+            return None
+    else:
+        return None
+#print(
+#    input_text_to_number(),
+#    input_text_to_number('8, 20, 30'),
+#    input_text_to_number('32, 54, 80', list_mode=False)
+#)
+#input()
+
+
+
+
+# Texto que sera reutilizado por varias funciones
 option_text_input = f'{ get_text("option") }: '
 
 
@@ -73,6 +142,77 @@ def menu_set_profile():
 
 
 '''
+Menu Seleccionar Capetas o archivos de Mods
+'''
+def menu_set_bool():
+    pass
+
+
+def menu_set_mods_files(profile=None, parameter=None):
+    menu_title = Title( f"{get_text('select')}: {get_text('mods_files')}", print_mode=False)
+    menu_options = ''
+    dict_mods_files = {}
+    number = 1
+    for mod in get_mods_files():
+        dict_mods_files.update( { number: mod } )
+        menu_options += f'{number}. {mod}\n'
+        number += 1
+
+    # Loop
+    loop = True
+    while loop:
+        CleanScreen()
+        option = input_text_to_number(
+            input(
+                menu_title +
+                menu_options +
+                option_text_input
+            )
+        )
+        if type(option) == list:
+            mode_list = True
+            selected_option = []
+            for number in option:
+                for key in dict_mods_files.keys():
+                    if number == key:
+                        selected_option.append( dict_mods_files[number] )
+        else:
+            mode_list = False
+            selected_option = None
+            if not(option) == None:
+                for key in dict_mods_files.keys():
+                    if option == key:
+                        selected_option = dict_mods_files[option]
+
+
+        # Establecer Archivos de Mods
+        if mode_list == True:
+            if not selected_option == []:
+                for mod in selected_option:
+                    set_profile_parameter_mod(
+                        profile=profile, parameter=parameter, mod_file=mod
+                    )
+        else:
+            if not selected_option == None:
+                set_profile_parameter_mod(
+                    profile=profile, parameter=parameter, mod_file=selected_option
+                )
+        print(
+            selected_option, '\n',
+            option
+        )
+        loop = False
+
+
+
+
+def menu_set_mods_dirs(profile=None):
+    pass
+
+
+
+
+'''
 Manu Configurar perfil
 '''
 def menu_config_profile():
@@ -119,9 +259,10 @@ def menu_config_profile():
                         if (
                             option == 'IgnoreFiles'
                         ):
-                            print( get_mods_files() )
+                            menu_set_mods_files(profile=profile, parameter=option)
                         else:
-                            print( get_mods_dirs() )
+                            for x in get_mods_dirs():
+                                print( x )
                         input( get_profile_parameter_listMods(profile=profile, parameter=option) )
                 else:
                     loop = False
@@ -167,8 +308,7 @@ while loop:
         option = dict_options[option]
         if option == 'set_profile':
             profile = menu_set_profile()
-            if not set_profile == None:
-                set_profile( profile=profile, text_ini=get_text_modloader() )
+            if not set_profile( profile=profile, text_ini=get_text_modloader() ) == None:
                 exec_and_exit = Continue(
                     get_current_profile(more_text=True) +
                     f'{get_text("exit_and_exec_game")}?'
