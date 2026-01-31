1c3f3333a7753a3241ba40b995985a1351abd218
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-01-30 20:38:06 -0600

    launcher fixes

--- core/launcher/simple_launcher.py
+++ core/launcher/simple_launcher.py
@@ -7,10 +7,20 @@ class SimpleLauncher:
     def __init__(self, file_path: pathlib.Path):
         self.file_path = file_path
         self.system = get_system() # simple string `win` or `linux`.
+        self.runner = None
 
     def launch(self):
-        if self.system == 'win':
-            return NativeRunner(self.file_path).run()
-        if self.system == 'linux':
-            return WineRunner(self.file_path).run()
-        raise RunTimeError('Unsupported system')
+        if self.runner is None:
+            if self.system == 'win':
+                self.runner = NativeRunner(self.file_path)
+            elif self.system == 'linux':
+                self.runner = WineRunner(self.file_path)
+            else:
+                raise RunTimeError('Unsupported system')
+            return self.runner.run()
+        else:
+            return False
+
+    def stop(self):
+        if self.runner:
+            self.runner.stop()
--- models/gta_sa_modloader/__init__.py
+++ models/gta_sa_modloader/__init__.py
@@ -1,3 +1,2 @@
-from .gta_sa_modloader_model import GTASAModloaderModel
 from .profile_model import ProfileModel
 from .folder_model import FolderModel
